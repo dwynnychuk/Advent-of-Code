42725827ddb4eb3a1a47dc59c61e529aa250699a
Dallyn Wynnychuk <dallynwynnychuk@Dallyns-MacBook-Pro-4.local>
2022-12-01 08:02:51 -0700

    day01 part01

---
+++ day01/part1.py
@@ -0,0 +1,13 @@
+# Day 1 or AOC 2022
+#import pandas as pd
+
+#data = pd.read_csv("day01/input.txt", header=None)
+#print(data.iloc[2:10])
+
+with open("day01/input.txt") as f:
+    data = f.read().splitlines()
+
+dataInt = []
+for d in data:
+    if
+print(dataInt)

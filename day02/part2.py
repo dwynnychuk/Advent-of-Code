5c6d32e3efc47d4cdca0e12b4616d89bca6eaef2
Dallyn Wynnychuk <dallynwynnychuk@Dallyns-MacBook-Pro-4.local>
2022-12-02 06:40:06 -0700

    Day 2 complete

---
+++ day02/part2.py
@@ -0,0 +1,46 @@
+# Day 2 of AOC 2022
+# 0 point X, 3 point Y, 6 point Z
+# 1 point Rock, 2 points Paper, 3 points Scissors
+
+with open("day02/input.txt") as f:
+    data = f.read().splitlines()
+
+elf = []
+play = []
+points = 0
+
+for d in data:
+    elf.append(d.split(" ")[0])
+    play.append(d.split(" ")[1])
+
+for i in range(len(elf)):
+    if play[i] == "X" and elf[i] == "A":
+        points += 0
+        points += 3
+    elif play[i] == "X" and elf[i] == "B":
+        points += 0
+        points += 1
+    elif play[i] == "X" and elf[i] == "C":
+        points += 0
+        points += 2
+    if play[i] == "Y" and elf[i] == "A":
+        points += 3
+        points += 1
+    elif play[i] == "Y" and elf[i] == "B":
+        points += 3
+        points += 2
+    elif play[i] == "Y" and elf[i] == "C":
+        points += 3
+        points += 3
+    if play[i] == "Z" and elf[i] == "A":
+        points += 6
+        points += 2
+    elif play[i] == "Z" and elf[i] == "B":
+        points += 6
+        points += 3
+    elif play[i] == "Z" and elf[i] == "C":
+        points += 6
+        points += 1
+
+print(points)
+        

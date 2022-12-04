e5b9c04a1bda2857daa76810e78070403ff19e1e
dwynnychuk <dwynnychuk@gmail.com>
2022-12-03 22:18:18 -0700

    part 1 complete

--- day04/part1.py
+++ day04/part1.py
@@ -2,4 +2,15 @@
 with open("day04/input.txt") as f:
     data = f.read().splitlines()
 
+contained = 0
 
+for d in data:
+    s1, s2 = d.split(",")
+    s1_s, s1_e = s1.split("-")
+    s2_s, s2_e = s2.split("-")
+    if int(s1_s) <= int(s2_s) and int(s1_e) >= int(s2_e):
+        contained += 1
+    elif int(s2_s) <= int(s1_s) and int(s2_e) >= int(s1_e):
+        contained += 1
+
+print(contained)

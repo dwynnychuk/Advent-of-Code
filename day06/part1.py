6924cd3b11bd612b31cf2f54ee324b7b2bed8f5f
dwynnychuk <dwynnychuk@gmail.com>
2022-12-06 07:06:41 -0700

    Part 1 complete

--- day06/part1.py
+++ day06/part1.py
@@ -1,3 +1,24 @@
 # AOC Day 6 2022
 with open("day06/input.txt") as f:
-    data = f.read().splitlines()
+    data = f.read()
+
+s = list(data)
+
+for d in range(4000):
+    four = s[d:(d+4)]
+    f_4 = four.pop()
+    if f_4 in four:
+        print("duplicate")
+        continue
+    f_3 = four.pop()
+    if f_3 in four:
+        print("triplicate")
+        continue
+    f_2 = four.pop()
+    if f_2 in four:
+        print("quadrupilcate")
+        continue
+    print(f"Passed {d+4}")
+    break
+    
+

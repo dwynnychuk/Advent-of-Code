2204d6c4d5fc6999cfb0b274b36679e2191bd81a
dwynnychuk <dwynnychuk@gmail.com>
2022-12-06 22:15:58 -0700

    Day 6 complete

--- day06/part2.py
+++ day06/part2.py
@@ -1,3 +1,21 @@
 # AOC Day 6 2022
 with open("day06/input.txt") as f:
-    data = f.read().splitlines()
+    data = f.read()
+
+lstr = 14
+s = list(data)
+answer = []
+
+for d in range(len(s)):
+    strip = s[d:(d+lstr)]
+    for p in range(len(strip)):
+        if strip.count(strip[p]) > 1:
+            break
+        elif p == (len(strip) - 1):
+            answer.append(d+lstr)
+            break
+
+print(answer)
+
+    
+

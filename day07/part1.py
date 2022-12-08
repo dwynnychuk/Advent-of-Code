d232cc9fed22719f0ae67b93ac9f43a88c388d01
dwynnychuk <dwynnychuk@gmail.com>
2022-12-08 07:25:38 -0700

    Part 1 day 7 complete

--- day07/part1.py
+++ day07/part1.py
@@ -1,47 +1,47 @@
 # AOC day 7 2022
+from ast import Add
+
+
 with open("day07/input.txt") as f:
     data = f.read().splitlines()
 
-# data = """$ cd /
-# $ ls
-# dir a
-# 14848514 b.txt
-# 8504156 c.dat
-# dir d
-# $ cd a
-# $ ls
-# dir e
-# 29116 f
-# 2557 g
-# 62596 h.lst
-# $ cd e
-# $ ls
-# 584 i
-# $ cd ..
-# $ cd ..
-# $ cd d
-# $ ls
-# 4060174 j
-# 8033020 d.log
-# 5626152 d.ext
-# 7214296 k"""
-
+def AddPath(path,dir):
+    if path not in dir.keys():
+        dir[path] = 0
+    return dir
 
 dirSize = {}
 folders = []
-curPath = []
+curPath = ""
 
 for line in data:
     if line.startswith("$ cd"):
         if line.startswith("$ cd /"):
-            pass
+            curPath = "/"
+            folders = ["/"]
+            dirSize = AddPath(curPath,dirSize)
+
         elif line.startswith("$ cd .."):
-            pass
+            curPath = "/".join(curPath.split("/")[:-1])
+            folders.pop()
+
         else:
-            pass
+            if curPath == "/":
+                curPath += line.split()[-1]
+            else:
+                curPath += "/" + line.split()[-1]
+            folders.append(curPath)
+            dirSize = AddPath(curPath,dirSize)
+
 
     if line[0].isdigit():
         fileSize = int(line.split(' ')[0])
         for dir in folders:
             dirSize[dir] += fileSize
 
+largeFiles = []
+for i in dirSize.values():
+    if i < 100000:
+        largeFiles.append(i)
+
+print(f"The sum of all files less than size 100,000 is {sum(largeFiles)}")

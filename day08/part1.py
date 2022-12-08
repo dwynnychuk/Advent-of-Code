6fbeb269b2e20b35762c34ba79eff3392c878f85
dwynnychuk <dwynnychuk@gmail.com>
2022-12-08 07:59:40 -0700

    Day 8 setup

---
+++ day08/part1.py
@@ -0,0 +1,14 @@
+# AOC 2022 day 8
+
+with open("day08/input.txt") as f:
+    data = f.read().splitlines()
+
+def CheckVisibility(row, column, value):
+    visible = True
+    return visible
+
+for r in data:
+    for c in r:
+        pass
+
+

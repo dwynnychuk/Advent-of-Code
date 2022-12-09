d7987b060d8c882d5442dadc2cf88d4fd7726e61
dwynnychuk <dwynnychuk@gmail.com>
2022-12-08 20:45:33 -0700

    Working on Day 8

--- day08/part1.py
+++ day08/part1.py
@@ -1,4 +1,5 @@
 # AOC 2022 day 8
+import numpy as np
 
 with open("day08/input.txt") as f:
     data = f.read().splitlines()
@@ -7,8 +8,11 @@ def CheckVisibility(row, column, value):
     visible = True
     return visible
 
-for r in data:
-    for c in r:
-        pass
+rows = len(data)
+cols = len(data[0])
+trees = np.zeros((rows,cols))
 
-
+for r in range(len(data)):
+    temp = list(data[r])
+    for c in range(len(temp)):
+        trees[r,c] = int(temp[c])

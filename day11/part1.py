f1dfd120ae7c26800a87c883b0f16e024d79abba
dwynnychuk <dwynnychuk@gmail.com>
2022-12-14 20:06:03 -0700

    Monkey class established

--- day11/part1.py
+++ day11/part1.py
@@ -1,8 +1,24 @@
 # AOC 2022 day 11
+import numpy as np
+
 with open("day11/input.txt") as f:
     data = f.read().splitlines()
+    
+num_monkeys = 8
+test_nums = []
+true_throw = []
+false_throw = []
+starting_items = []
 
-print(data)
+for d in data:
+    if "Test: divisible by" in d:
+        test_nums.append(int(d.split()[-1]))
+    elif "If true:" in d:
+        true_throw.append(int(d.split()[-1]))
+    elif "If false:" in d:
+        false_throw.append(int(d.split()[-1]))
+    elif "Starting" in d:
+        starting_items.append(d.split(sep=": ")[-1])
 
 class Monkey:
     def __init__(self, id):
@@ -11,7 +27,5 @@ class Monkey:
     def __str__(self) -> str:
         return f"Monkey #{self.id}"
 
-m1 = Monkey(1)
+monkeys = [Monkey(i) for i in range(num_monkeys)]
 
-print(m1)
-print(m1.id)

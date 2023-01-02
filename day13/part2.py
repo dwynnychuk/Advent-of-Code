d788201ee391a5702f837bb0ad13e9d67b988ddd
dwynnychuk <dwynnychuk@gmail.com>
2023-01-02 10:07:21 -0700

    Part 2 testing

--- day13/part2.py
+++ day13/part2.py
@@ -1,29 +1,20 @@
 # AOC 2022 day 13
+# go through all packets and find those less than 2 and those less than 6
 with open("day13/input.txt") as f:
     data = f.read().strip().split("\n\n")
 
 packets = []
-for ind, packet in enumerate(data):
+
+for packet in data:
     left_right = packet.split("\n")
-    packets.append((ind, eval(left_right[0]), eval(left_right[1])))
+    packets.append(eval(left_right[0]))
+    packets.append(eval(left_right[1]))
 
-def compare(l, r):
-    if isinstance(l, int) and isinstance(r, list):
-        l = [l]
-    if isinstance(l, list) and isinstance(r, int):
-        r = [r]
-    
-    if isinstance(l, int) and isinstance(r, int):
-        if l < r:
-            return 1
-        if l == r:
-            return 0
-        return -1
-
-    if isinstance(l, list) and isinstance(r, list):
+def compare(first, lessthan2, lessthan6):
+    if isinstance(first, list):
         count = 0
-        while count < len(l) and count < len(r):
-            result = compare(l[count],r[count])
+        while count < len(first):
+            result = compare(first[count], lessthan2, lessthan6)
 
             if result == 1:
                 return 1
@@ -38,12 +29,25 @@ def compare(l, r):
             return 1
 
         return -1
+        
+    if isinstance(first, int):
+        if first == 6:
+            print("6")
+        if first < 6:
+            lessthan6 += 1
+            if first < 2:
+                lessthan2 += 1
+            if first == 2:
+                print("2")
+            return 1
+    return -1
 
-correct = 0
+lessthan2 = 1
+lessthan6 = 1
 
-for pack in packets:
-    l, r = pack[1], pack[2]
-    if compare(l,r) == 1:
-        correct += pack[0] + 1
+# for pack in packets:
+#     l, r = pack[1], pack[2]
+#     if compare(l,r) == 1:
+#         correct += pack[0] + 1
 
-print(f"The sum of correctly ordered packets is {correct}")
+# print(f"The sum of correctly ordered packets is {correct}")

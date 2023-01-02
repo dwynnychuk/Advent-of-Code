eae423e493e019a83c84a61f9ed0ef37f7bdd52d
dwynnychuk <dwynnychuk@gmail.com>
2023-01-02 14:09:57 -0700

    Day 13 complete

--- day13/part1.py
+++ day13/part1.py
@@ -1,11 +1,13 @@
 # AOC 2022 day 13
+from functools import cmp_to_key
+
+
 with open("day13/input.txt") as f:
-    data = f.read().strip().split("\n\n")
+    data = f.read().strip().replace("\n\n", "\n").split("\n")
 
 packets = []
-for ind, packet in enumerate(data):
-    left_right = packet.split("\n")
-    packets.append((ind, eval(left_right[0]), eval(left_right[1])))
+for packet in data:
+    packets.append(eval(packet))
 
 def compare(l, r):
     if isinstance(l, int) and isinstance(r, list):
@@ -39,11 +41,13 @@ def compare(l, r):
 
         return -1
 
-correct = 0
+packets.append([[2]])
+packets.append([[6]])
+packets = sorted(packets, key=cmp_to_key(compare), reverse = True)
+for index, pack in enumerate(packets):
+    if pack == [[2]]:
+        lessthan2 = index + 1
+    if pack == [[6]]:
+        lessthan6 = index + 1
 
-for pack in packets:
-    l, r = pack[1], pack[2]
-    if compare(l,r) == 1:
-        correct += pack[0] + 1
-
-print(f"The sum of correctly ordered packets is {correct}")
+print(f"\nThe index of [[2]] and [[6]] are {lessthan2} and {lessthan6} with a product of {lessthan2 * lessthan6}\n")
--- day13/part2.py
+++ day13/part2.py
@@ -1,5 +1,8 @@
 # AOC 2022 day 13
 # go through all packets and find those less than 2 and those less than 6
+from xml.etree.ElementTree import iselement
+
+
 with open("day13/input.txt") as f:
     data = f.read().strip().split("\n\n")
 
@@ -12,24 +15,13 @@ for packet in data:
 
 def compare(first, lessthan2, lessthan6):
     if isinstance(first, list):
-        count = 0
-        while count < len(first):
-            result = compare(first[count], lessthan2, lessthan6)
+        if first:
+            lessthan2, lessthan6 = compare(first[0], lessthan2, lessthan6)
+        else:
+            lessthan2 += 1
+            lessthan6 += 1
+            return lessthan2, lessthan6
 
-            if result == 1:
-                return 1
-            if result == -1:
-                return -1
-
-            count += 1
-
-        if count == len(l):
-            if len(l) == len(r):
-                return 0
-            return 1
-
-        return -1
-        
     if isinstance(first, int):
         if first == 6:
             print("6")
@@ -39,15 +31,11 @@ def compare(first, lessthan2, lessthan6):
                 lessthan2 += 1
             if first == 2:
                 print("2")
-            return 1
-    return -1
+            return lessthan2, lessthan6
 
 lessthan2 = 1
 lessthan6 = 1
+for pack in packets:
+    lessthan2, lessthan6 = compare(pack, lessthan2, lessthan6)
 
-# for pack in packets:
-#     l, r = pack[1], pack[2]
-#     if compare(l,r) == 1:
-#         correct += pack[0] + 1
-
-# print(f"The sum of correctly ordered packets is {correct}")
+print(f"The number of packets less than 2 and 6 is {lessthan2} and {lessthan6} giving product {lessthan2 * lessthan6}")

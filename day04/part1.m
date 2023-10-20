162cf85df2bbf1c7cce249af162f6c19744180cc
dwynnychuk <dwynnychuk@gmail.com>
2023-10-19 21:56:06 -0600

    Day 4 complete - matlab

--- day04/part1.m
+++ day04/part1.m
@@ -16,9 +16,23 @@ fclose(fileID);
 
 
 numPairs = length(lines);
+encapsulated = 0;
 
 for i = 1:numPairs
     pair = split(lines(i),',');
     elf1 = pair(1,1);
     elf2 = pair(2,1);
+    
+    elf1Lims = split(elf1,'-');
+    elf2Lims = split(elf2,'-');
+
+    elf1Low = str2double(elf1Lims(1,1));
+    elf1High = str2double(elf1Lims(2,1));
+
+    elf2Low = str2double(elf2Lims(1,1));
+    elf2High = str2double(elf2Lims(2,1));
+
+    if ((elf1Low <= elf2Low) && (elf1High >= elf2High)) || ((elf2Low <= elf1Low) && (elf2High >= elf1High))
+        encapsulated = encapsulated + 1;
+    end
 end

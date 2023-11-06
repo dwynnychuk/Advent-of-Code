3ec329ec9777d492acf1a5e8fa9c2a4cbfee1260
dwynnychuk <dwynnychuk@gmail.com>
2023-11-05 19:02:35 -0700

    Day 01 p 1 cpp

--- day01/part1.cpp
+++ day01/part1.cpp
@@ -3,8 +3,19 @@
 #include <string>
 using namespace std;
 
-int main()
-{
-    fstream newfile;
-    newfile.open("input.txt",ios::in);    
+int main() {
+std::string line;
+std::fstream file("input.txt");
+int max = 0;
+int total = 0;
+while (std::getline(file,line)) {
+    if (line.size() == 0) {
+        max = std::max(max, total);
+        total = 0;
+    } else {
+        total += std::stoi(line);
+    }
+}
+max = std::max(total,max);
+printf("Max is equal to: %i\n",max);
 }
--- day05/part2.m
+++ day05/part2.m
@@ -2,3 +2,114 @@
 % Day 5
 % Part 2
 
+clear
+clc
+
+% File reading
+% Initial crate stacks
+fileID = fopen('input.txt', 'r');
+lines = strings(0, 1);
+line = fgetl(fileID);
+for i = 1:8
+    lines(end+1) = line;
+    line = fgetl(fileID);
+end
+fclose(fileID);
+clear i fileID line
+
+stacknum = 9;
+maxstackheight = 8;
+
+% Determine length of array
+for i = 1:maxstackheight
+    maxl = length(lines{1,i});
+    if length(lines{1,i}) > maxl
+        maxl = length(lines{1,i});
+    end
+end
+clear i maxl
+idxchar = 2:4:34;
+stacks = strings(maxstackheight,stacknum);
+
+% Make Crate Stacks
+for i = 1:maxstackheight
+    for j = 1:stacknum
+        stacks(i,j) = lines{1,i}(idxchar(j));
+    end
+end
+clear i j lines idxchar
+
+% extend stacks
+ogstack = maxstackheight;
+maxstackheight = 100;
+temp = stacks;
+stacks = strings(maxstackheight,stacknum);
+stacks((maxstackheight-ogstack+1):end,:) = temp;
+
+% Begin parsing data
+rowOne = 11;
+fileID = fopen('input.txt', 'r');
+lines = strings(0, 1);
+line = fgetl(fileID);
+linecounter = 1;
+while ischar(line)
+    if linecounter >= rowOne
+        lines(end+1) = line;
+    end
+    line = fgetl(fileID);
+    linecounter = linecounter + 1;
+end
+fclose(fileID);
+clear fileID linecounter line rowOne
+
+% make arrays from data
+movecount = length(lines);
+
+fromArray = zeros(1,movecount);
+toArray = zeros(1,movecount);
+numMoves = zeros(1,movecount);
+
+for i = 1:movecount
+    splitarray = split(lines(i));
+    numMoves(i) = str2num(splitarray(2));
+    fromArray(i) = str2num(splitarray(4));
+    toArray(i) = str2num(splitarray(6));
+end
+clear i ans lines splitarray
+
+%% Computation
+
+for i = 1:movecount
+    for k = 1:size(stacks,1)
+        if ~(strcmp(stacks(k,fromArray(i)),' ') | strcmp(stacks(k,fromArray(i)),''))
+            moveLetter = stacks(k:(k+numMoves(i)),fromArray(i));
+            stacks(k,fromArray(i)) = ' ';%%%%%%%%%%%%%%%%%%%%%%%
+            break
+        end
+    end
+
+    for l = 1:maxstackheight
+        if l == maxstackheight
+            stacks(l,toArray(i)) = moveLetter;
+            break
+        elseif (strcmp(stacks(l,toArray(i)),' ') | strcmp(stacks(l,toArray(i)),'')) ...
+                && ~(strcmp(stacks(l+1,toArray(i)),' ') | strcmp(stacks(l+1,toArray(i)),''))
+            stacks(l,toArray(i)) = moveLetter;
+            break
+        end
+    end
+end
+clear i j k l moveLetter fromArray toArray temp ogstack movecount stacknum maxstackheight
+
+% Result
+topCrates = '';
+for i = 1:size(stacks,2)
+    for j = 1:size(stacks,1)
+        if isletter(stacks(j,i))
+            topCrates = append(topCrates,stacks(j,i));
+            break
+        end
+    end
+end
+
+fprintf('\nThe value of the top crates are: %s\n\n',topCrates)

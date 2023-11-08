eab44496c91295a77d972257a8e4e510ef574b85
dwynnychuk <dwynnychuk@gmail.com>
2023-11-07 20:17:02 -0700

    AOC 2022 Day 3 p 1 C++

--- day03/part1.cpp
+++ day03/part1.cpp
@@ -11,28 +11,36 @@ int main() {
     std::string line;
     std::string lhs;
     std::string rhs;
-    char pointReference[52][1] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-                                'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 
-                                'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
-                                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    std::string pointReference("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
     char tempChar;
 
     int lineLength;
     int rucksackQuantity;
+    int tempIdx;
+    int tempScore;
+    int totalScore;
 
     vector<char> commons;
+    vector<int> scores;
 
     while (std::getline(file,line)) {
         lineLength = line.length();
         rucksackQuantity = lineLength / 2;
+        lhs = line.substr(0, rucksackQuantity);
+        rhs = line.substr(rucksackQuantity, lineLength);
 
-        for () {
-            
+        for (int i = 0; i < rucksackQuantity; i++) {
+            tempChar = lhs[i];
+            if (rhs.find(tempChar) != string::npos) {
+                commons.push_back(tempChar);
+                break;
+            }
         }
+        tempIdx = pointReference.find(tempChar);
+        totalScore += (tempIdx + 1);
+
     }
 
-    //int totalScore = std::accumulate(scores.begin(), scores.end(), 0);
-
-    //printf("The Total Score is: %i\n\n", totalScore);
+    std::cout << "\nThe total score is: " << totalScore << "\n\n";
 }

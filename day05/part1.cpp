2508a898ada28f0c6f2315ae963a383051dbf169
dwynnychuk <dwynnychuk@gmail.com>
2023-11-12 20:27:18 -0700

    AOC 2022 Day 5 P 1 - in progress

--- day05/part1.cpp
+++ day05/part1.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <fstream>
 #include <stack>
+#include <vector>
+#include <regex>
 using namespace std;
 
 int main() {
@@ -22,14 +24,25 @@ int main() {
     std::vector<std::stack<char> > stacks(stack_num);
     for (int i = input_height-1; i >= 0; i--) {
         for (int j = 0; j < stack_num; j++){
-            // do something
+            const int k = (j+1)*4 - 3;
+            if (initial_condition[i][k] == ' ') {
+                continue;
+            } else {
+                stacks[j].push(initial_condition[i][k]);
+            }
         }
     }
 
-
+std::getline(file,line);
+const std::regex pattern("move ([0-9]+) from ([0-9]) to ([0-9])");
     while (std::getline(file,line)) {
+        // Turn line into 3 columns using regex
+        std::smatch match;
+        std::regex_match(line, match, pattern);
 
-        // Turn line into 3 columns
+        const int quantity = std::stoi(match[1]);
+        const int from = std::stoi(match[2]);
+        const int to = std::stoi(match[3]);
 
     }
 

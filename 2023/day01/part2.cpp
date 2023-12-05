e7133e7c28d1c2d2acf66aff5831f2b6281bd053
dwynnychuk <dwynnychuk@gmail.com>
2023-12-04 21:49:52 -0700

    2023 day 1 part 2 logic building

---
+++ 2023/day01/part2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <numeric>
+using namespace std;
+
+int main() {
+    std::fstream file("input.txt");
+    std::string line;
+    std::string linenum;
+    std::string digits;
+
+    int totalsum;
+    int linelength;
+    int count;
+    int numdigits = 9;
+
+    std::string WrittenNumbers[9] = 
+        {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    
+    std::string ReplaceNumbers[9] = 
+        {"o1e", "t2o", "t3e", "f4r", "f5e", "s6x", "s7n", "e8t", "n9e"};
+
+    while (std::getline(file,line)) {
+        count += 1;
+
+        for (int i = 0; i < numdigits; i++) {
+            line.replace(WrittenNumbers[i],ReplaceNumbers[i]);
+        }
+
+        linelength = line.length();
+        digits = "";
+
+        for (int i = 0; i < linelength; i++) {
+            if (isdigit(line[i])) {
+                digits += line[i];
+            }
+        }
+
+        linenum = digits.substr(0,1) + digits.substr(digits.length()-1, digits.length());
+        totalsum += std::stoi(linenum);
+    }
+
+    std::cout << "\nThe total sum is: " << totalsum << "\n\n";
+}

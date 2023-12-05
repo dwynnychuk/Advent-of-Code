d9ab274ad2bb3fea4409d0dc529c42b4a2a09021
dwynnychuk <dwynnychuk@gmail.com>
2023-12-04 20:32:37 -0700

    2023 Day 1 Part 1 cpp

--- 2023/day01/part1.cpp
+++ 2023/day01/part1.cpp
@@ -11,34 +11,25 @@ int main() {
     std::fstream file("input.txt");
     std::string line;
     std::string linenum;
+    std::string digits;
 
     int totalsum;
     int linelength;
-    int lnum;
-    char d1;
-    char d2;
+    int count;
 
     while (std::getline(file,line)) {
         linelength = line.length();
-        linenum = "";
-        lnum += 1;
+        count += 1;
+        digits = "";
 
         for (int i = 0; i < linelength; i++) {
             if (isdigit(line[i])) {
-                d1 = line[i];
-                break;
+                digits += line[i];
             }
         }
-        linenum += d1;
-        for (int i = linelength; i > 0; i--) {
-            if (isdigit(line[i])) {
-                d2 = line[i];
-                break;
-            }
-        }  
-        linenum += d2;
+
+        linenum = digits.substr(0,1) + digits.substr(digits.length()-1, digits.length());
         totalsum += std::stoi(linenum);
-        std::cout << lnum << " " << linenum << " " << totalsum << "\n";
     }
 
     std::cout << "\nThe total sum is: " << totalsum << "\n\n";

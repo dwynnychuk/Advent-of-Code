c338b2f40c2c2534d7252449c3f3e4fd808dad4e
dwynnychuk <dwynnychuk@gmail.com>
2023-12-04 19:45:05 -0700

    Logic bug, Day 1 P 1

--- 2023/day01/part1.cpp
+++ 2023/day01/part1.cpp
@@ -10,22 +10,35 @@ using namespace std;
 int main() {
     std::fstream file("input.txt");
     std::string line;
+    std::string linenum;
 
-    int digits;
     int totalsum;
-    int linenum;
     int linelength;
+    int lnum;
+    char d1;
+    char d2;
 
     while (std::getline(file,line)) {
-        digits = 0;
         linelength = line.length();
+        linenum = "";
+        lnum += 1;
 
         for (int i = 0; i < linelength; i++) {
             if (isdigit(line[i])) {
-                digits += 1;
+                d1 = line[i];
+                break;
             }
         }
-        std::cout << digits << "\n";
+        linenum += d1;
+        for (int i = linelength; i > 0; i--) {
+            if (isdigit(line[i])) {
+                d2 = line[i];
+                break;
+            }
+        }  
+        linenum += d2;
+        totalsum += std::stoi(linenum);
+        std::cout << lnum << " " << linenum << " " << totalsum << "\n";
     }
 
     std::cout << "\nThe total sum is: " << totalsum << "\n\n";

0a0d463fbea8a25876615e593a7483eb0470a98f
dwynnychuk <dwynnychuk@gmail.com>
2025-06-22 18:41:11 +0900

    AOC2022 day 05 recompleted

--- 2022/day05/part1.py
+++ 2022/day05/part1.py
@@ -1,65 +1,31 @@
 #Day 05 of AOC2022
-with open("day05/input.txt") as f:
-    data = f.read().splitlines()
 
-initialEnd = 8
-dataStart = 10
+class Solution:
+    def __init__(self, datafile):
+        with open(datafile) as f:
+            self.data = f.read().splitlines()
+            
+        self.stacks = {f"S{k}": [] for k in range(1,10,1)}
+        for i in range(7,-1,-1):
+            for j in range(1,10,1):
+                if self.data[i][(j-1)*4+1] != " ":
+                    self.stacks[f"S{j}"].append(self.data[i][(j-1)*4+1])
 
-one = []
-two = []
-three = []
-four = []
-five = []
-six = []
-seven = []
-eight = []
-nine = []
+    def solvep1(self):
+        output = []
+        for move in self.data[10::]:
+            _, num, _, fromstack, _, tostack = move.split(" ")
+            self.move_item(int(num), int(fromstack), int(tostack))
+        
+        for val in self.stacks.values():
+            output.append(val[-1])
+        return output
+                        
+    def move_item(self, num, fromstack, tostack):
+        for _ in range(num):
+            temp = self.stacks[f"S{fromstack}"].pop()
+            self.stacks[f"S{tostack}"].append(temp)
 
-for d in data[0:initialEnd]:
-    one.append(d[1:2])
-    two.append(d[5:6])
-    three.append(d[9:10])
-    four.append(d[13:14])
-    five.append(d[17:18])
-    six.append(d[21:22])
-    seven.append(d[25:26])
-    eight.append(d[29:30])
-    nine.append(d[33:34])
-
-one.reverse()
-two.reverse()
-three.reverse()
-four.reverse()
-five.reverse()
-six.reverse()
-seven.reverse()
-eight.reverse()
-nine.reverse()
-
-array = [one, two, three, four, five, six, seven, eight, nine]
-for r in array:
-    while(" " in r):
-        r.remove(" ")
-
-quantity = []
-old = []
-new = []
-
-for l in data[dataStart:]:
-    _, q, _, o, _, n = l.split(" ")
-    quantity.append(q)
-    old.append(o)
-    new.append(n)
-
-for lines in range(len(quantity)):
-    fRow = int(old[lines]) - 1
-    tRow = int(new[lines]) - 1
-    for q in range(int(quantity[lines])):
-        transfer = array[fRow].pop()
-        array[tRow].append(transfer)
-
-winner = []
-
-for w in range(9):
-    winner.append(array[w][-1])
-print(winner)
+p1 = Solution("day05/input.txt")
+print(f"Day 5 part 1 answer: {p1.solvep1()}")
+        

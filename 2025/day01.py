f89b4a5331ae165edbc1d4927a8eff04becb78d0
dwynnychuk <dwynnychuk@gmail.com>
2025-12-02 19:48:38 -0800

    AOC 2025 day 1 complete

--- 2025/day01.py
+++ 2025/day01.py
@@ -8,13 +8,18 @@ class Day01:
             self.ends_at_num_count = 0
             self.num = 0
     
-    def _check_limits(self) -> None:
-        print(self.position)
+    def _check_limits_1(self) -> None:
         self.position = self.position % 100
         if self.position == self.num:
             self.ends_at_num_count += 1
-        print(self.position)
-        print("\n")
+    
+    def _check_limits_2(self) -> None:
+        if (self.position < self.lower_limit) or (self.position > self.upper_limit):
+            self.ends_at_num_count += max((abs(self.position) // 100),1)
+            self.position = self.position % 100
+        
+        if self.position == self.num:
+            self.ends_at_num_count += 1
     
     def part1(self):
         for line in self.data:
@@ -22,18 +27,26 @@ class Day01:
             magnitude = int(line[1:])
             if direction == "L":
                 self.position -= magnitude
-                self._check_limits()
+                self._check_limits_1()
             elif direction == "R":
                 self.position += magnitude
-                self._check_limits()
+                self._check_limits_1()
             else:
                 raise ValueError("Invalid direction")
         print(f"The password is: {self.ends_at_num_count}")
             
     def part2(self):
-        pass
-    
+        for line in self.data:
+            direction = line[0]
+            magnitude = int(line[1:])
+            
+            step = -1 if direction == "L" else 1
+            for _ in range(magnitude):
+                self.position = (self.position + step) % 100
+                if self.position == self.num:
+                    self.ends_at_num_count +=1
+        print(f"The password is: {self.ends_at_num_count}")
     
 if __name__ == "__main__":
     day01 = Day01()
-    day01.part1()
+    day01.part2()

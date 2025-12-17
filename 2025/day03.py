510a0be057ef18c200d2d12f51bcf80341b782ea
dwynnychuk <dwynnychuk@gmail.com>
2025-12-16 20:28:37 -0800

    AOC 2025 Day 3 setup

---
+++ 2025/day03.py
@@ -0,0 +1,15 @@
+class Day03:
+    def __init__(self):
+        with open("2025/input_day03.txt") as f:
+            data = f.read().splitlines()
+            
+    def solve_p1(self) -> None:
+        pass
+    
+    def solve_p2(self) -> None:
+        pass
+    
+if __name__ == "__main__":
+    problem = Day03
+    problem.solve_p1()
+    problem.solve_p2()

2ca71bd01eb58ab5f9b223defc8d62a8715ee489
dwynnychuk <dwynnychuk@gmail.com>
2025-12-16 21:26:09 -0800

    AOC 2025 Day 3 part 1 complete

--- 2025/day03.py
+++ 2025/day03.py
@@ -1,15 +1,27 @@
 class Day03:
     def __init__(self):
         with open("2025/input_day03.txt") as f:
-            data = f.read().splitlines()
+            self.data = f.read().splitlines()
             
     def solve_p1(self) -> None:
-        pass
+        total_output = 0
+        for bank in self.data:
+            total_output += self._get_max_joltage(bank, 2)
+        print(f"The total output joltage for part one is: {total_output}")
     
     def solve_p2(self) -> None:
         pass
     
+    def _get_max_joltage(self, bank: str, num_batteries: int) -> int:
+        l = len(bank)
+        max = int(bank[0:num_batteries])
+        for idx1, i in enumerate(bank[0:-1:]):
+            for j in bank[idx1+1::]:
+                if int(str(i+j)) > max:
+                    max = int(str(i+j))
+        return max
+    
 if __name__ == "__main__":
-    problem = Day03
+    problem = Day03()
     problem.solve_p1()
     problem.solve_p2()
